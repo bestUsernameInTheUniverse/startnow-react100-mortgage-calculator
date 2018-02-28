bc11975e2f78603a2167076abea6d3f89f0b37cc
yurik <yuri.kromsky@gmail.com>
2018-02-27 19:23:05 -0800

    replaced fat arrows with binds, and consolidated event handlers

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,46 +3,36 @@ import React from 'react';
 export default class App extends React.Component {
   constructor(props){
     super(props);
+
     this.state = {
-      loanBalance: 0,
-      interestRate: 0,
-      loanTerm: 15,
+      balance: 0,
+      rate: 0,
+      term: 15,
       output: 0
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    document.title = "Yuri's Mortgage Calculator";
-  }
-
-  handleLoanBalanceChange(event) {
-    this.setState({ loanBalance: event.target.value });
-  }
-
-  handleInterestRateChange(event) {
-    this.setState({ interestRate: event.target.value });
-  }
-
-  handleLoanTermChange(event) {
-    this.setState({ loanTerm: event.target.value })
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   
   handleSubmit(event) {
-    var result = this.mortgageCalculatorMath();
     event.preventDefault();
+    var result = this.mortgageCalculatorMath();
     this.setState({ output: result })
   }
 
   mortgageCalculatorMath() {
-    var M, P, r, n;
+    const P = parseFloat(this.state.balance);
+    const r = parseFloat(this.state.rate)/100/12;
+    const n = parseInt(this.state.term)*12;
 
-    P = parseFloat(this.state.loanBalance);
-    r = parseFloat(this.state.interestRate)/100/12;
-    n = parseInt(this.state.loanTerm)*12;
-
-    console.log(P, r, n);
-
-    M = P * (r * ((1 + r) ** n)) / (((1 + r) ** n) - 1)
+    const M = P * (r * ((1 + r) ** n)) / (((1 + r) ** n) - 1)
 
     return Math.ceil(M * 100) / 100;
   }
@@ -50,39 +40,28 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
+        <form onSubmit={this.handleSubmit}>
           <h1>Mortgage Calculator</h1>
           
           <label className="form-group">
             Loan Balance
-            <input 
-              name="balance"
-              className="form-control"
-              type="text" 
-              value={this.state.loanBalance} 
-              onChange={(e) => this.handleLoanBalanceChange(e)}
-            />
+            <input name="balance" className="form-control" type="text" value={this.state.loanBalance} onChange={this.handleChange}/>
           </label>
 
           <label>
             Interest Rate (%)
-            <input
-              name="rate"
-              type="text" 
-              value={this.state.interestRate} 
-              onChange={(e) => this.handleInterestRateChange(e)}
-            />
+            <input name="rate" type="text" value={this.state.interestRate} onChange={this.handleChange}/>
           </label>
 
           <label>
             Loan Term (years)
-            <select name="term" value={this.state.loanTerm} onChange={(e) => this.handleLoanTermChange(e)}>
+            <select name="term" value={this.state.loanTerm} onChange={this.handleChange}>
               <option value="15">15</option>
               <option value="30">30</option>
             </select>
           </label>
 
-          <button name="submit" className="btn btn-primary">Calculate</button>
+          <button type="submit" className="btn btn-primary">Calculate</button>
         </form>
 
         <div id="output" className="output">
